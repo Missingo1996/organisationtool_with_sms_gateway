a03c382e4262d7d75598795b65f7551679e5b47c
Missingo1996 <f.plaimauer@gmail.com>
2015-09-26 00:04:11 +0200

    Test

--- test.php
+++ test.php
@@ -1,7 +1,3 @@
 <?php
-include 'includes/db_config.php';
-$DATABASE = DATABASE;
-$create_database = "CREATE DATABASE IF NOT EXISTS $DATABASE";
-echo $create_database;
-echo "<br />Datenbank <b>$DATABASE </b> wurde erfolgreich angelegt.";
+echo  $_SERVER['HTTP_HOST'];
 ?>

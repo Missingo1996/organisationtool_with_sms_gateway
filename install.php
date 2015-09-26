5bc4235e344eb464fe346989ff6ec5eb8863a27b
Missingo1996 <f.plaimauer@gmail.com>
2015-09-26 16:07:00 +0200

    added tables and admincheck

--- install.php
+++ install.php
@@ -1,4 +1,9 @@
 <?php
+session_start();
+if (!isset($_SESSION["install"]))
+{
+//MySQL INSTALLIEREN
+
 include 'includes/db_config.php';
 
 // Verbindungsvariable mit Zugangsdaten festlegen
@@ -31,7 +36,7 @@ else{
 echo "<br /><br /><h3>Tabellen werden angelegt...</h3>";
 
 //USER-Tabelle
-$table_user = "CREATE TABLE IF NOT EXISTS user (id INT(8) NOT NULL AUTO_INCREMENT, username VARCHAR(50) DEFAULT NULL, password VARCHAR(512) DEFAULT NULL, firstname VARCHAR(50) DEFAULT NULL, lastname VARCHAR(50) DEFAULT NULL, mail VARCHAR(150) DEFAULT NULL, course VARCHAR(20) DEFAULT NULL, PRIMARY KEY(id))";
+$table_user = "CREATE TABLE IF NOT EXISTS user (id INT(8) NOT NULL AUTO_INCREMENT, username VARCHAR(50) DEFAULT NULL, password VARCHAR(512) DEFAULT NULL, permissions INT(3) DEFAULT NULL, firstname VARCHAR(50) DEFAULT NULL, lastname VARCHAR(50) DEFAULT NULL, mail VARCHAR(150) DEFAULT NULL, course VARCHAR(20) DEFAULT NULL, PRIMARY KEY(id))";
 if($db->query($table_user)){
 	echo "Tabelle: <b>user</b> wurde erfolgreich angelegt";
 }
@@ -40,7 +45,29 @@ else{
 }
 
 //STUDENTS-Tabelle
+$table_students = "CREATE TABLE IF NOT EXISTS students (id INT(8) NOT NULL AUTO_INCREMENT, firstname VARCHAR(50) DEFAULT NULL, lastname VARCHAR(50) DEFAULT NULL, phone VARCHAR(30) DEFAULT NULL, ort VARCHAR(80) DEFAULT NULL, course VARCHAR(20) DEFAULT NULL, lastchange VARCHAR(100) DEFAULT NULL, PRIMARY KEY(id))";
+if($db->query($table_students)){
+	echo "Tabelle: <b>students</b> wurde erfolgreich angelegt";
+}
+else{
+	echo "Tabelle: <b>students</b> konnte nicht angelegt werden";
+}
+//COURSE-Tabelle
+$table_course = "CREATE TABLE IF NOT EXISTS course (id (INT(8) NOT NULL AUTO_INCREMENT, name VARCHAR(50) DEFAULT NULL, PRIMARY KEY(id))";
+if($db->query($table_course)){
+	echo "Tabelle: <b>course</b> wurde erfolgreich angelegt";
+}
+else{
+	echo "Tabelle: <b>course</b> konnte nicht angelegt werden";
+}
 
+//ADMINISTRATOR-ACCOUNT ANLEGEN
+$result = $db->query("SELECT id FROM user WHERE permissions LIKE '0'");
+$admin = $result->num_rows;
 
+if($admin == 0)
+	{
+	//Admin-Formular ausgeben
+	}
 $db->close();
 ?>

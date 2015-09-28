cd9959f57d887d4a2c0c9fa82a9ce0cbe2a18e16
Missingo1996 <f.plaimauer@gmail.com>
2015-09-28 21:21:42 +0200

    debugging

--- install.php
+++ install.php
@@ -1,10 +1,9 @@
 <?php
 session_start();
 
-if (!isset($_SESSION["install"]))
+if(!isset($_SESSION["install"]))
 {
 //MySQL INSTALLIEREN
-
 include 'includes/db_config.php';
 
 // Verbindungsvariable mit Zugangsdaten festlegen
@@ -39,27 +38,27 @@ echo "<br /><br /><h3>Tabellen werden angelegt...</h3>";
 //TEACHER-Tabelle
 $table_teacher = "CREATE TABLE IF NOT EXISTS teacher (id INT(8) NOT NULL AUTO_INCREMENT, username VARCHAR(50) DEFAULT NULL, password VARCHAR(512) DEFAULT NULL, permissions INT(3) DEFAULT NULL, firstname VARCHAR(50) DEFAULT NULL, lastname VARCHAR(50) DEFAULT NULL, mail VARCHAR(150) DEFAULT NULL, course VARCHAR(20) DEFAULT NULL, PRIMARY KEY(id))";
 if($db->query($table_teacher)){
-	echo "Tabelle: <b>teacher</b> wurde erfolgreich angelegt";
+	echo "Tabelle: <b>teacher</b> wurde erfolgreich angelegt<br />";
 }
 else{
-	echo "Tabelle: <b>teacher</b> konnte nicht angelegt werden";
+	echo "Tabelle: <b>teacher</b> konnte nicht angelegt werden<br />";
 }
 
 //STUDENTS-Tabelle
 $table_students = "CREATE TABLE IF NOT EXISTS students (id INT(8) NOT NULL AUTO_INCREMENT, firstname VARCHAR(50) DEFAULT NULL, lastname VARCHAR(50) DEFAULT NULL, phone VARCHAR(30) DEFAULT NULL, ort VARCHAR(80) DEFAULT NULL, course VARCHAR(20) DEFAULT NULL, lastchange VARCHAR(100) DEFAULT NULL, language VARCHAR(3) DEFAULT NULL, PRIMARY KEY(id))";
 if($db->query($table_students)){
-	echo "Tabelle: <b>students</b> wurde erfolgreich angelegt";
+	echo "Tabelle: <b>students</b> wurde erfolgreich angelegt<br />";
 }
 else{
-	echo "Tabelle: <b>students</b> konnte nicht angelegt werden";
+	echo "Tabelle: <b>students</b> konnte nicht angelegt werden<br />";
 }
 //COURSE-Tabelle
 $table_course = "CREATE TABLE IF NOT EXISTS course (id INT(8) NOT NULL AUTO_INCREMENT, name VARCHAR(50) DEFAULT NULL, time INT(8) DEFAULT NULL, PRIMARY KEY(id))";
 if($db->query($table_course)){
-	echo "Tabelle: <b>course</b> wurde erfolgreich angelegt";
+	echo "Tabelle: <b>course</b> wurde erfolgreich angelegt<br />";
 }
 else{
-	echo "Tabelle: <b>course</b> konnte nicht angelegt werden";
+	echo "Tabelle: <b>course</b> konnte nicht angelegt werden<br />";
 }
 
 $db->close();
@@ -75,7 +74,11 @@ include 'includes/db_connect.php';
 
 if($_SESSION["install"] == 1){
 //ADMINISTRATOR-ACCOUNT ANLEGEN
-$result = $db->query("SELECT id FROM user WHERE permissions LIKE '0'");
+$result = $db->query("SELECT id FROM user WHERE permissions = 3");
+
+// SELECT FROM termin ORDER BY time ASC
+
+
 $admin = $result->num_rows;
 
 	if($admin == 0)
@@ -84,32 +87,33 @@ $admin = $result->num_rows;
 	$_SESSION["install"] = 2;
 	//Formular
 	?>
-		<form action="register.php" method="post">
-		Dein Username:<br>
-		<input type="text" size="24" maxlength="50" name="username"><br><br>
+		<form action="#" method="post">
+		Dein Username:<br />
+		<input type="text" size="24" maxlength="50" name="username"><br /><br />
 
-		Dein Passwort:<br>
-		<input type="password" size="24" maxlength="50" name="password"><br>
+		Dein Passwort:<br />
+		<input type="password" size="24" maxlength="50" name="password"><br />
 
-		Passwort wiederholen:<br>
-		<input type="password" size="24" maxlength="50" name="password2"><br>
+		Passwort wiederholen:<br />
+		<input type="password" size="24" maxlength="50" name="password2"><br />
 		
-		Dein Vorname:<br>
-		<input type="text" size="24" maxlength="50" name="firstname"><br><br>
+		Dein Vorname:<br />
+		<input type="text" size="24" maxlength="50" name="firstname"><br /><br />
 		
-		Dein Nachname:<br>
-		<input type="text" size="24" maxlength="50" name="lastname"><br><br>
+		Dein Nachname:<br />
+		<input type="text" size="24" maxlength="50" name="lastname"><br /><br />
 		
-		Deine E-Mail-Adresse:<br>
-		<input type="text" size="24" maxlength="50" name="mail"><br><br>
+		Deine E-Mail-Adresse:<br />
+		<input type="text" size="24" maxlength="50" name="mail"><br /><br />
 
 		<input type="submit" value="Abschicken">
 		</form>
 	<?php
 	}
+exit();
 }
 else{
-	session_destroy;
+	session_destroy();
 }
 
 

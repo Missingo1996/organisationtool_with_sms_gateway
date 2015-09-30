db1ba96cdc8580309c0093f7c8d9e179623a9246
Missingo1996 <f.plaimauer@gmail.com>
2015-09-30 13:06:43 +0200

    added working registerform

--- login/register.php
+++ login/register.php
@@ -7,7 +7,7 @@ $registerform = "
 Benutzername:<br />
 <input type=\"text\" size=\"24\" maxlength=\"50\" name=\"username\"><br />
 
-E-Mail::<br />
+E-Mail:<br />
 <input type=\"text\" size=\"24\" maxlength=\"50\" name=\"mail\"><br /><br />
 
 Vorname:<br />
@@ -32,6 +32,65 @@ if(!isset($_SESSION["username"]) && (!isset($_POST["username"]) || !isset($_POST
 {
 	echo $registerform;
 	exit();
+}
+
+// Wenn angemeldet
+if(isset($_SESSION["username"]))
+{
+	$url = $_SERVER['HTTP_HOST'];
+	$uri   = rtrim(dirname(dirname($_SERVER['PHP_SELF'])), '/\\');
+	header("Location: ../index.php");
+	exit();
+}
+
+//Bei fehlerhafter Formulareingabe
+if($_POST["username"] == "" || $_POST["password"] == "" || $_POST["password2"] == "" || $_POST["firstname"] == "" || $_POST["lastname"] == "" || $_POST["mail"] == "" || $_POST["password"] != $_POST["password2"])
+{
+	echo $registerform;
+	echo "<p style=\"color: red\">Bitte alle Felder korrekt ausfüllen.</p>";
+}
+elseif(!filter_var($_POST["mail"], FILTER_VALIDATE_EMAIL))
+{
+	echo $registerform;
+	echo "<p style=\"color: red\">Ungültige E-Mail-Adresse.</p>"; 
+}
+else
+{
+	require_once '../includes/db_connect.php';
+	require_once './encrypt.php';
+	
+	$username = $_POST["username"];
+	$password = encryptPassword($username, $_POST["password"]);
+	$firstname = $_POST["firstname"];
+	$lastname = $_POST["lastname"];
+	$mail = $_POST["mail"];
+	
+	//Account bereits vorhanden?
+	$result = $db->query("SELECT id FROM teacher WHERE username LIKE '$username' OR mail LIKE '$mail'");
+	$rowCnt = $result->num_rows;
+	
+	if($rowCnt == 0)
+	{
+		//Wenn USER oder MAIL nicht vorhanden, eintragen
+		$register = $db->query("INSERT INTO teacher (username, password, firstname, lastname, mail, permissions) VALUES ('$username', '$password', '$firstname', '$lastname', '$mail', '0')");
+		
+		//Eintrag erfolgreich
+		if($register)
+		{
+			echo 	"Sie haben sich erfolgreich registriert.<br />
+					Ihr Benutzername lautet <b>$username</b>.<br /><br />
+					Zum Login? <a href=\"./login.php\">Login</a>";
+		}
+		else
+		{
+			echo "Registrieren fehlgeschlagen. Bitte wenden Sie sich an einen Administrator";
+		}
+	}
+	else
+	{
+		echo $registerform;
+		echo "<p style=\"color: red\">Benutzername und/oder E-Mail-Adresse schon vorhanden.</p>"; 
+	}
 }
 
 ?>
